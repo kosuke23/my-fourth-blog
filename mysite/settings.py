da3cb736f51cf43f9421983ba9f56692c283200a
kosuke <inokuchi.kosuke@gmail.com>
2020-07-18 08:54:12 +0900

    My Django Girls app, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib.auth import views
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
